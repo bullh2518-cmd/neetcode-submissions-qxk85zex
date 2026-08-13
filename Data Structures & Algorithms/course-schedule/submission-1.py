0f312c90ca9a170fc4a9521eab3cf0b3e5966dd4
bullh2518-cmd <bullh2518@gmail.com>
2026-08-13 10:29:08 -0500

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g = defaultdict(list)
+
+        for a, b in prerequisites:
+            g[a].append(b)
+
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+
+        states = [UNVISITED] * numCourses
+
+        def dfs(node):
+            state = states[node]
+
+            # Already completely checked this node
+            if state == VISITED:
+                return True
+
+            # We came back to a node currently in our DFS path
+            if state == VISITING:
+                return False
+
+            # Mark this node as currently being explored
+            states[node] = VISITING
+
+            for nei in g[node]:
+                if not dfs(nei):
+                    return False
+
+            # Finished exploring this node successfully
+            states[node] = VISITED
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
